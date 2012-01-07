77308af4ea07bc0a6a07c4aa04632f68f9af1e67
Benjamin Kirchner <benjamin.kirchner@mni.thm.de>
2012-01-07 22:20:03 +0100

    Added source to system message

--- src/util/system_message.py
+++ src/util/system_message.py
@@ -7,9 +7,5 @@ from util.message import Message
 
 class SystemMessage(Message):
     
-    def __init__(self, target, body):
-        Message.__init__(self, None, target, body)
-    
-    def __str__ (self):
-        rep = self._Target +" | "+ self._Message_Body
-        return rep
+    def __init__(self, source, target, body):
+        Message.__init__(self, source, target, body)
