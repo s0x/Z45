be96382089f59e5e2d65ab0e5528dbb1ccaaa15f
jx47 <jx47@gmx.de>
2012-01-07 19:04:14 +0100

    Now with content

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -3,3 +3,19 @@ Created on 07.01.2012
 
 @author: Marcel M.
 '''
+
+class Bot (object):
+    def __init__ (self):
+        return False
+    
+    def new_connection (self):
+        return False
+    
+    def listen (self, msg):
+        return False
+    
+    def send (self, msg):
+        return False
+    
+    def callback (self, function):
+        return False
